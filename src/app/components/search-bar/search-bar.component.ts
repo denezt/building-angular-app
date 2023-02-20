e8fb8f3015a6218b8d3c07637b20a7c6d33468b0
Richard Jackson <Richard.jackson@quanto-solutions.de>
2023-02-20 22:54:42 +0100

    Updating application routing, adding search bar and home components

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  /**
+   *
+   */
+  constructor(private router: Router) { 
+
+    }
+
+  ngOnInit():void {  }
+
+  onSubmit(form: NgForm){
+    this.router.navigate(['search', form.value.search]);
+  }
 
 }
